a95a9efe189111dcb6d123549f493e6bdfa4423e
mattgoldsmith <goldsmithmj91@gmail.com>
2019-12-11 15:33:40 +0700

    Add project files.

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Owner = name;
+            Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("John", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+
+        }
+    }
+}
