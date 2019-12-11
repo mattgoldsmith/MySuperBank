81224b1ca7b90a0e019d862448137c4929292d04
mattgoldsmith <goldsmithmj91@gmail.com>
2019-12-11 16:32:40 +0700

    Added logging to the withdrawal transactions End of C# 101

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace MySuperBank
 {
@@ -56,5 +57,19 @@ namespace MySuperBank
             var withdrawal = new Transaction(-amount, date, note);
             allTransactions.Add(withdrawal);
         }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
     }
 }
+
